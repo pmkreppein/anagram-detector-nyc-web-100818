3a0c30bce25bd72cf7e2fc6c02fdf80e94965424
Peter Kreppein <pmkreppein@gmail.com>
2018-10-10 20:33:44 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,8 @@ class Anagram
   end   
 
   def match(input)
-    word_array = input.split
-    word_array.split("").sort == @name.split("").sort
+    word_array = input.split("")
+    word_array.sort == @name.split("").sort
     
   end   
 

ca3ff5708ebcd63b87998ffed9836eeb9287d231
Peter Kreppein <pmkreppein@gmail.com>
2018-10-10 20:43:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :name
+def initialize(word)
+    @name = word 
+  end   
+  def match(anagrams)
+    anagrams.select {|item| item.split("").sort == @name.split("").sort}
+  end   
+end
+
+   

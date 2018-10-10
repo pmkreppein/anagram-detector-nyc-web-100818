9ef0675925552b0b261fe09ad184f32818341cbe
Peter Kreppein <pmkreppein@gmail.com>
2018-10-10 20:33:14 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,10 @@ class Anagram
     @name = word 
   end   
 
-  def match(array)
-    array.select {|x| x.split("").sort == @name.split("").sort}
+  def match(input)
+    word_array = input.split
+    word_array.split("").sort == @name.split("").sort
+    
   end   
 
 end
